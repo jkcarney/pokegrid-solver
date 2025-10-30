2959bf1316d8470e36111d45a472f20ee2ed8021
Joshua Carney <Joshua.Carney@jhuapl.edu>
2025-10-30 10:18:39 -0400

    added reverse chatgpt strategy for a baseline floor

--- src/pokegrid_solver/strategy.py
+++ src/pokegrid_solver/strategy.py
@@ -55,3 +55,16 @@ class ChatGPTStrategy(PokegridStrategy):
         ranked_tuple_list = self.ranked_list_to_tuples(gpt_ranked_list)
         ranked_tuple_list.sort(key=lambda t: (-t[1], t[0]))
         return ranked_tuple_list
+
+
+class ReverseChatGPTBaselineStrategy(ChatGPTStrategy):
+    def __init__(self, model="gpt-5-mini-2025-08-07"):
+        super().__init__(model=model)
+        self._system_prompt = \
+        """
+        You are a Pokemon game expert. You will receive a set of Pokemon names. You must rank each Pokemon by how popular you think that Pokemon is. Ie, what is the probability or likelihood that that Pokemon is popular among the Pokemon crowd, fanbase, or general collective conciousness.
+        Your job is to return a list of tuples, where each tuple is a string, float where the string is the Pokemon's name as I originally gave it and the float is the score for that Pokemon.
+        Higher floats mean that Pokemon is more popular, and therefore more desirable. Lower floats mean that Pokemon is less popular and much less desirable.
+        Don't worry about ordering the list, only return it in the same order I provided you with the floats attached as tuples.
+        """
+        self._agent = Agent(model, output_type=RankedList, system_prompt=self._system_prompt)
